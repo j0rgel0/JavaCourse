f58def5f270eee4875144899da6068734391b6e6
jorge.lopezvz <jorge.lopezvz@udlap.mx>
2023-05-29 17:50:30 -0600

    Add code for calling HelloWorld class

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import com.a1.firststeps.HelloWorld;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create an instance of the HelloWorld class
+        HelloWorld hello = new HelloWorld();
+
+        // Call the printHelloWorld method on the hello instance
+        hello.printHelloWorld();
+    }
+}
---
+++ src/com/a1/firststeps/HelloWorld.java
@@ -0,0 +1,17 @@
+// Package Names:
+// - Package names should be written in lowercase letters.
+// - Package names should be a series of words separated by periods (e.g., com.example.package).
+// - Package names should be unique and follow the reverse domain name convention to avoid naming conflicts.
+package com.a1.firststeps;
+
+// Class Names:
+// - Class names should be written in CamelCase.
+// - The first letter of the class name should be capitalized.
+// - If the class name consists of multiple words, capitalize the first letter of each word (e.g., ClassName).
+// - Class names should be descriptive and meaningful, representing the purpose or nature of the class.
+public class HelloWorld {
+        // Print "Hello, World!" to the console
+        public void printHelloWorld() {
+            System.out.println("Hello, World!");
+        }
+}
