89da12a1a52259db430b1c955936192721911d50
jorge.lopezvz <jorge.lopezvz@udlap.mx>
2023-06-10 14:55:21 -0600

    Variabes and Methods in Java

--- src/com/a1/firststeps/HelloWorld.java
+++ src/com/a1/firststeps/HelloWorld.java
@@ -1,7 +1,7 @@
 // Classes are the basic building blocks of Java programs.
 // When defining a class, you describe its parts and characteristics.
 
-// Package Names:
+// Package Names. It prevents naming collisions and access protection.
 // - Package names should be written in lowercase letters.
 // - Package names should be a series of words separated by periods (e.g., com.example.package).
 // - Package names should be unique and follow the reverse domain name convention to avoid naming conflicts.
@@ -13,20 +13,28 @@ package com.a1.firststeps;
 // - If the class name consists of multiple words, capitalize the first letter of each word (e.g., ClassName).
 // - Class names should be descriptive and meaningful, representing the purpose or nature of the class.
 public class HelloWorld {
-    // Fields (Variables)
-    // Fields store program state and represent the components of a class.
+
+    // Fields (Variables). Fields store program state and represent the components of a class.
     // Declare and define variables to store data that the class needs.
+    // Field names should be written in camelCase.
+    // The first letter of the field name should be lowercase.
+    // If the field name consists of multiple words, capitalize the first letter of each word except the first one.
+    // Field names should be descriptive and indicate the purpose or nature of the field.
+
     // For example, we can declare a field called "counter" to store a count value.
     private int counter;
 
-    // Methods (Functions/Procedures)
-    // Methods perform operations on program state and represent the behavior of a class.
+    // Methods (Functions/Procedures). Methods perform operations on program state and represent the behavior of a class.
     // Implement functionality by writing code inside methods.
+    // Method names should be written in camelCase.
+    // The first letter of the method name should be lowercase.
+    // If the method name consists of multiple words, capitalize the first letter of each word except the first one.
+    // Method names should be descriptive and indicate the action performed by the method.
+
     // For example, we can define a method called "incrementCounter" to increase the value of the counter field.
     public void incrementCounter() {
         counter++;
     }
-
     public void printHelloWorld() {
         System.out.println("Hello, World!"); // Print "Hello, World!" to the console
     }
@@ -35,8 +43,6 @@ public class HelloWorld {
         HelloWorld hello = new HelloWorld(); // Create an instance of the HelloWorld class
         hello.printHelloWorld(); // Call the printHelloWorld method, which prints "Hello, World!"
         hello.incrementCounter(); // Call the incrementCounter method, which increments the value of counter
-
-        // Print the value of counter to the console
-        System.out.println("Counter value: " + hello.counter);
+        System.out.println("Counter value: " + hello.counter); // Print the value of counter to the console
     }
 }
