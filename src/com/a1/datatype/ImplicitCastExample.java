6a1fc042e64a48f7956089f84c31cbb9ca00afba
jorge.lopezvz <jorge.lopezvz@udlap.mx>
2023-06-11 13:58:08 -0600

    Adding examples of Explicit cast, Implicit cast, Infinity, NaN, Long and Wrapper Class

---
+++ src/com/a1/datatype/ImplicitCastExample.java
@@ -0,0 +1,10 @@
+package com.a1.datatype;
+public class ImplicitCastExample {
+    public static void main(String[] args) {
+        int myInt = 10;
+        double myDouble = myInt; // Implicit cast from int to double
+
+        System.out.println("Original int value: " + myInt);
+        System.out.println("Widened double value: " + myDouble);
+    }
+}
